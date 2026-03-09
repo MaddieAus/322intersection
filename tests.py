451f7104d29f2a31cb210a2de78cf111f583e23f
Madison Austin <maustin3@mtech.edu>
2026-03-09 12:23:49 -0600

    modified main and tests

--- tests.py
+++ tests.py
@@ -1,17 +1,42 @@
+"""
+Madison Grace Austin
+CSCI 332 Spring 2025
+Programming Assignment # class 16 tests.py
+I acknowledge that I have worked on this assignment independently, except where explicitly
+noted and referenced. Any collaboration or use of external resources has been properly cited.
+I am fully aware of the consequences of academic dishonesty and agree to abide by the
+university's academic integrity policy. I understand the importance the consequences of
+plagiarism.
+"""
+
 import unittest
-from main import add_positive_integers
+from main import get_orientation#, do_intersect
+class TestOrientation(unittest.TestCase):
 
-class TestMathFunctions(unittest.TestCase):
-    def test_sum_correct(self):
-        self.assertEqual(add_positive_integers(5, 10), 15)
+    def test_collinear(self):
+        self.assertEqual(
+            get_orientation((0, 0), (2, 2), (4, 4)),
+            0
+    )
+    def test_clockwise(self):
+        self.assertEqual(
+            get_orientation((0, 0),(1, 2), (4, 4)),
+            1
+    )
+    def test_counterclockwise(self):
+        self.assertEqual(
+            get_orientation((0, 0), (4, 4), (2, 5)),
+            2
+    )
+    # def test_segments_intersect(self):
+        # seg1 = ((1,1),(4,4))
+        # seg2 = ((2,4),(4,2))
+        # self.assertTrue(do_intersect(seg1, seg2))
 
-    def test_negative_input(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers(-1, 5)
-
-    def test_wrong_type(self):
-        with self.assertRaises(ValueError):
-            add_positive_integers("5", 5)
+    # def test_segments_do_not_intersect(self):
+        # seg1 = ((1,1),(2,2))
+        # seg2 = ((3,3),(4,4))
+        # self.assertFalse(do_intersect(seg1, seg2))
 
 if __name__ == "__main__":
     unittest.main()
